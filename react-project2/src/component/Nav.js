611e3f2bb4f68412cb4883a13ccba68c82ee900a
hyung-2 <nh_0221@naver.com>
2023-09-19 17:17:37 +0900

    로고추가

--- react-project2/src/component/Nav.js
+++ react-project2/src/component/Nav.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from './Button'
 import './Nav.css'
-
+import logo from './logo.png'
 
 function Nav(){
   return(
     <div className={`Nav`}>
-      <Button>버튼</Button>
+    <img src={logo}></img>
+      {/* <Button>버튼</Button> */}
     </div>
   )
 }
