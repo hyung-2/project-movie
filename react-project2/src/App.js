6ea90c48167fab56ce111ad4dfe1a77d0884c985
hyung-2 <nh_0221@naver.com>
2023-09-19 17:56:26 +0900

    hyung, sun 브랜치 merge후 충돌수정

--- react-project2/src/App.js
+++ react-project2/src/App.js
@@ -2,26 +2,11 @@ import './App.css';
 import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Homepage, MoreGenre } from './pages'
-
-<<<<<<< HEAD
-import { Route, Routes } from 'react-router-dom';
 import { Main, Tournament, Winner } from './pages'
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Tournament></Tournament> */}
-      <Routes>
-        <Route exact path='/' element={<Main/>}/>
-        <Route exact path='/tournament' element={<Tournament/>}/>
-        <Route exact path='/result' element={<Winner/>}/>
-      </Routes>
-    </div>
-  );
-=======
 class App extends Component {
   homeMenu = [
-    {url: '/', name: 'Homepage'},
+    {url: '/home', name: 'Homepage'},
     {url: '/more', name: 'MoreGenre'},
   ]
 
@@ -34,7 +19,10 @@ class App extends Component {
     return (
       <div className="App">
         <Routes>
-          <Route exact path='/' element={<Homepage/>}></Route>
+          <Route exact path='/' element={<Main/>}/>
+          <Route exact path='/tournament' element={<Tournament/>}/>
+          <Route exact path='/result' element={<Winner/>}/>
+          <Route exact path='/home' element={<Homepage/>}></Route>
           <Route exact path='/more' element={<MoreGenre/>}>
             <Route path=':genreId' element={<MoreGenre/>}/>
 
@@ -44,7 +32,6 @@ class App extends Component {
     );
 
   }
->>>>>>> 611e3f2bb4f68412cb4883a13ccba68c82ee900a
 }
 
 export default App;
--- react-project2/src/pages/index.js
+++ react-project2/src/pages/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 export { default as Main } from './Main'
 export { default as Tournament } from './Tournament'
 export { default as Winner } from './Winner'
-=======
 export { default as Homepage } from './Homepage'
 export { default as MoreGenre } from './MoreGenre'
->>>>>>> 611e3f2bb4f68412cb4883a13ccba68c82ee900a
